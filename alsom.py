f116542be8b64c1442638ae6396ad010b8ab7cdf
nrrnie <72966907+nrrnie@users.noreply.github.com>
2023-06-15 23:36:30 +0600

    Add files via upload

--- alsom.py
+++ alsom.py
@@ -1,25 +1,59 @@
 import sys
 import argparse
 import librosa
+import musdb
+import ffmpeg
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from IPython.display import Audio
 
-def main(argv):
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument('-f', '--file', help='The path to the audio file', required='True')
+def downloadMUSDB():
+    train = musdb.DB(download=True, subsets='train', split=None)
+    test = musdb.DB(download=True, subsets='test', split=None)
+    return train, test
 
-    args = argParser.parse_args()
-    print(args.file)
+def spec(dataset, targets=False):
+    return 
+
+def make_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape = (1025, 587)),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dense(5, activation='softmax')
+    ])
+
+    return model
+
+def train_model(model):
+    model.fit()    
+
+
+    return
+
+def main():
+
+    short_train, short_test = downloadMUSDB()
+
+    mus = musdb.DB(
+        root = r'./musdb/',
+    )
+
     
-    y, sr = librosa.core.load(args.file)
-    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
-    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
-    librosa.display.specshow(spectrogram, x_axis='time',
-                                   y_axis='mel', sr=sr,
-                                   fmax=8000)
-    plt.colorbar()
-    plt.set_cmap('hot_r')
-    plt.show()
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+
+    #model = make_model()
+
+    #model.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError, metrics=['acc'])
+
+    print("ready")
+
+
+
+if __name__ == '__main__':
+    main()
